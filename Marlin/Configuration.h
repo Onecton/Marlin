26cc93032a1c60875a8b33122faee57f96f82c7e
Dario Ernst <dario@kanojo.de>
2019-10-01 20:17:07 +0200

    Reduce bedsize so tilting doesn‘t crash

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1037,8 +1037,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 299
-#define Y_BED_SIZE 299
+#define X_BED_SIZE 297
+#define Y_BED_SIZE 297
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -20
